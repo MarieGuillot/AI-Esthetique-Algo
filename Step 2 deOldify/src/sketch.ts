8950fd91f2a98c5561dca80bc3f88fa9d6983035
Sirine302 <64043568+Sirine302@users.noreply.github.com>
2021-05-10 20:05:55 +0200

    Add files via upload

---
+++ Step 2 deOldify/src/sketch.ts
@@ -0,0 +1,65 @@
+// -------------------
+//  Parameters and UI
+// -------------------
+let tmpImage: p5.Image
+function preload() {
+    tmpImage = loadImage("img/5.png")
+}
+//const base64Image = tmpImage.canvas.toDataURL()
+
+
+const gui = new dat.GUI()
+const params = {
+    Ellipse_Size: 30,
+    Download_Image: () => save(),
+}
+gui.add(params, "Ellipse_Size", 0, 100, 1)
+gui.add(params, "Download_Image")
+
+const ai = new rw.HostedModel({
+    url: "https://deoldify-adc4e64d.hosted-models.runwayml.cloud/v1",
+  token: "PKOqb8Tnp0znteIEhJqdTw==",
+ });
+
+// -------------------
+//       Drawing
+// -------------------
+
+
+function draw() {
+    if (outImage)
+    image(outImage, 0, 0, width, height)
+}
+
+// -------------------
+//    Initialization
+// -------------------
+
+let img: p5.Element
+//const z = []
+
+let outImage: p5.Element
+
+ function setup() {
+    p6_CreateCanvas()
+    tmpImage.loadPixels()
+    const base64Image = tmpImage.canvas.toDataURL()
+    
+
+    const inputs = {
+        "image": base64Image,
+        "render_factor": 20
+    };
+
+
+    ai.query(inputs).then(outputs => {
+        const { image  } = outputs;
+        outImage=createImg(image);
+        outImage.hide()
+      });
+    }
+
+
+function windowResized() {
+    p6_ResizeCanvas()
+}
